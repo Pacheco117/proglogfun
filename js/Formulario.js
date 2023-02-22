5a622ec7e66f07970f9c51013859f222e6e4a902
Pacheco117 <68613915+Pacheco117@users.noreply.github.com>
2023-02-22 17:06:03 -0600

    Validacion hecha

--- js/Formulario.js
+++ js/Formulario.js
@@ -6,7 +6,7 @@ const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
     calle: /^[a-zA-ZÀ-ÿ\s]{2,4}$/,
     cruzamientos: /^[a-zA-ZÀ-ÿ\s]$/,
     referencia: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
